b105598408f4265bbbeb675641a28943e9c6f91b
davut.keskin <davut.keskin@cloudnesil.com>
2021-02-13 15:48:09 +0300

    tests added and clean refactoring

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
     <h2>{{ title }}</h2>
+    <label>
+      <input type="text" data-testid="todo-input" v-model="newTodo"/>
+    </label>
+    <button data-testid="todo-submit" @click.prevent="addTodo">Add</button>
+    <div data-testid="todos">
+      <div v-for="(todo,todoKey) of todos" :key="todoKey">
+        {{todo}}
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,6 +20,17 @@ export default {
     title: {
       type: String,
       required: true
+    }
+  },
+  data () {
+    return {
+      todos: [],
+      newTodo: ''
+    }
+  },
+  methods: {
+    addTodo () {
+      this.todos.push(this.newTodo)
     }
   }
 }
--- tests/unit/Todo.spec.js
+++ tests/unit/Todo.spec.js
@@ -2,8 +2,21 @@ import { shallowMount } from '@vue/test-utils'
 import Todo from '@/components/Todo'
 
 describe('The Todo.vue component', () => {
+  let wrapper
+  beforeEach(() => {
+    wrapper = shallowMount(Todo, {
+      propsData: {
+        title: 'My List'
+      }
+    })
+  })
+
+  async function addTodo (todoText) {
+    wrapper.find('[data-testid="todo-input"]').setValue(todoText)
+    await wrapper.find('[data-testid="todo-submit"]').trigger('click')
+  }
+
   it('can be mounted', () => {
-    const wrapper = shallowMount(Todo)
     expect(wrapper.exists()).toBeTruthy()
   })
 
@@ -14,5 +27,18 @@ describe('The Todo.vue component', () => {
       }
     })
     expect(wrapper.text()).toMatch('Props passed title')
+  })
+
+  it('allows for adding one todo item', async () => {
+    wrapper.find('[data-testid="todo-input"]').setValue('my first todo item')
+    await wrapper.find('[data-testid="todo-submit"]').trigger('click')
+    expect(wrapper.find('[data-testid="todos"]').text()).toContain('my first todo item')
+  })
+
+  it('allows for more than  one todo item to be added', async () => {
+    await addTodo('my first todo item')
+    await addTodo('my second todo item')
+    expect(wrapper.find('[data-testid="todos"]').text()).toContain('my first todo item')
+    expect(wrapper.find('[data-testid="todos"]').text()).toContain('my second todo item')
   })
 })
