08a537c311c321a9365a8f6d5b3894dd2aa1645e
davut.keskin <davut.keskin@cloudnesil.com>
2021-02-14 11:50:22 +0300

    test logic changed

--- tests/unit/store/todo.spec.js
+++ tests/unit/store/todo.spec.js
@@ -3,6 +3,6 @@ import todo from '@/store/todo'
 describe('The todo store', () => {
   it('uses a function to generate the initial state', () => {
     const newState = todo.state()
-    expect(newState).not.toBeDefined()
+    expect(newState).not.toBeUndefined()
   })
 })
